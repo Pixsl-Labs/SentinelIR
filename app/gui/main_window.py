1d14722c3b6a1a1c7c648c5bba46201e07d8e1e7
Samuel Stacey <staceys4@coventry.ac.uk>
2026-06-09 13:57:42 +0100

    Added shortcuts + simple temporary functions for the different actions File menu bar can do. Later will implement more menu bar options and shortcuts where need be!

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -4,16 +4,22 @@ Main GUI window for SentinelIR.
 
 # python3 -m app.gui.gui_main
 
+import sys
 from pathlib import Path
 
 from PySide6.QtWidgets import (
     QLabel,
     QMainWindow,
-    QStatusBar,
+    QStatusBar
 )
 
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QAction, QIcon
+from PySide6.QtGui import (
+    QAction,
+    QIcon,
+    QKeySequence,
+    QShortcut
+)
 
 from app.gui.styles.light_theme import LIGHT_THEME
 
@@ -46,6 +52,28 @@ class MainWindow(QMainWindow):
 
         self.setStatusBar(QStatusBar(self))
 
+        # --- Shortcuts ---
+
+        # --- Open Log File Shortcut ---
+
+        self.open_file_shortcut = QShortcut(QKeySequence("Ctrl+O"), self)
+        self.open_file_shortcut.activated.connect(lambda: self.load_log_file("auth.log"))
+
+        # --- Save Shortcut ---
+
+        self.save_shortcut = QShortcut(QKeySequence("Ctrl+S"), self)
+        self.save_shortcut.activated.connect(lambda: self.save("auth.log"))
+
+        # --- Export Report Shortcut ---
+
+        self.export_report_shortcut = QShortcut(QKeySequence("Ctrl+E"), self)
+        self.export_report_shortcut.activated.connect(lambda: self.save("auth.log"))
+
+        # --- Exit Shortcut ---
+
+        self.exit_shortcut = QShortcut(QKeySequence("Ctrl+Q"), self)
+        self.exit_shortcut.activated.connect(lambda: self.exit_application())
+
         # --- Menu Bar ---
 
         menu = self.menuBar()
@@ -69,8 +97,6 @@ class MainWindow(QMainWindow):
         file_menu.addAction(
             open_log_action
         )
-
-        file_menu.addSeparator()
 
         # --- Open Config File ---
 
@@ -135,14 +161,12 @@ class MainWindow(QMainWindow):
         )
 
         exit_action.triggered.connect(
-            self.menu_action_clicked
+            self.exit_application
         )
 
         file_menu.addAction(
             exit_action
         )
-
-        file_menu.addSeparator()
 
     def get_icon(
             self,
@@ -179,3 +203,63 @@ class MainWindow(QMainWindow):
             print(
                 f"Clicked: {action.text()}"
             )
+
+    def load_log_file(
+            self,
+            file_path:str
+    ) -> None:
+        """
+        Loads log file for analysis.
+
+        Args:
+            file_path (str): File path of file to open.
+
+        Returns:
+            None
+        """
+
+        print(f"Loading: {file_path}")
+
+    def save(
+                self,
+                file_path: str
+        ) -> None:
+        """
+        Saves current config file.
+
+        Args:
+            file_path (str): File path to save.
+
+        Returns:
+            None
+        """
+
+        print(f"Saved: {file_path}")
+
+    def export_report(
+            self,
+            file_path: str,
+            output_path: str
+    ) -> None:
+        """
+        Exports a report of the choosen / current log file.
+
+        Args:
+            file_path (str): File path to export to a report.
+            output_path (str): Output path for the exported report.
+
+        Returns:
+            None
+        """
+
+        print(f"Exporting report: {file_path} as {output_path}")
+
+    def exit_application(self) -> None:
+        """
+        Exits SentinelIR GUI.
+
+        Returns:
+            None
+        """
+
+        self.close()

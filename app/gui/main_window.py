a3cec2150be63154bdcf6ce4fba8955604d6fbaa
Samuel Stacey <staceys4@coventry.ac.uk>
2026-06-09 12:00:46 +0100

    Initial basic GUI following the first link within gui_links.md. Moving onto adding different features to the menu bar!

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -2,14 +2,18 @@
 Main GUI window for SentinelIR.
 """
 
+# python3 -m app.gui.gui_main
+
+from pathlib import Path
+
 from PySide6.QtWidgets import (
-    QMainWindow,
     QLabel,
-    QWidget,
-    QVBoxLayout
+    QMainWindow,
+    QStatusBar,
 )
 
 from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QIcon
 
 
 class MainWindow(QMainWindow):
@@ -30,13 +34,85 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("SentinelIR")
         self.setMinimumSize(900, 600)
 
+        self.assets_path = Path(__file__).resolve().parent / "assets"
+
         label = QLabel("Hello World - SentinelIR GUI")
-        label.setAlignment(Qt.AlignCenter)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        layout = QVBoxLayout()
-        layout.addWidget(label)
+        self.setCentralWidget(label)
 
-        container = QWidget()
-        container.setLayout(layout)
+        self.setStatusBar(QStatusBar(self))
 
-        self.setCentralWidget(container)
+        # --- Menu Bar ---
+
+        menu = self.menuBar()
+
+        # --- File Menu ---
+
+        file_menu = menu.addMenu("&File")
+
+        open_action = QAction(
+            QIcon(self.get_icon("file_icon.png")),
+            "Open",
+            self
+        )
+
+        open_action.triggered.connect(
+            self.menu_action_clicked
+        )
+
+        file_menu.addAction(
+            open_action
+        )
+
+        file_menu.addSeparator()
+
+        close_action = QAction(
+            QIcon(self.get_icon("close_icon.png")),
+            "Close",
+            self
+        )
+
+        close_action.triggered.connect(
+            self.menu_action_clicked
+        )
+
+        file_menu.addAction(
+            close_action
+        )
+
+    def get_icon(
+            self,
+            icon_name: str
+    ) -> QIcon:
+        """
+        Returns an icon from the GUI assets directory.
+
+        Args:
+            icon_name (str): Icon filename inside /app/gui/assets.
+
+        Returns:
+            QIcon: Loaded Qt icon.
+        """
+
+        return QIcon(
+            str(self.assets_path / icon_name)
+        )
+
+    def menu_action_clicked(
+            self
+    ) -> None:
+        """
+        Handles menu action clicks.
+
+        Returns:
+            None
+        """
+
+        action = self.sender()
+
+        if action:
+
+            print(
+                f"Clicked: {action.text()}"
+            )
---
+++ docs/gui_links.md
@@ -0,0 +1,3 @@
+# GUI Links / Tutorials used
+
+1. https://www.pythonguis.com/tutorials/pyside6-actions-toolbars-menus/
